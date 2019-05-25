450e3a3f7083bc77b3ec3e3bb26d59fc952055cd
Shuvo Sarker <shuvooa707@gmail.com>
2019-05-25 21:54:41 +0600

    added todolist.sql

--- database/todolist.sql
+++ database/todolist.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 08, 2019 at 10:55 PM
+-- Generation Time: May 25, 2019 at 03:21 PM
 -- Server version: 10.1.37-MariaDB
 -- PHP Version: 7.3.0
 
@@ -25,28 +25,84 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `list`
+--
+
+CREATE TABLE `list` (
+  `list_name` varchar(10000) NOT NULL,
+  `username` varchar(200) NOT NULL,
+  `primary_key` mediumtext NOT NULL,
+  `is_selected` int(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `list`
+--
+
+INSERT INTO `list` (`list_name`, `username`, `primary_key`, `is_selected`) VALUES
+('Goals 2019', 'shuvo', '2019454859', 0),
+('Things To Buy', 'shuvo', '2019454928', 0),
+('Projects To Do', 'shuvo', '2019461158', 0),
+('Places To Visit', 'shuvo', '2019461435', 0),
+('Movies To Download', 'shuvo', '2019452038', 1);
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `tasks`
 --
 
 CREATE TABLE `tasks` (
   `username` varchar(100) NOT NULL,
   `taskname` varchar(200) NOT NULL,
-  `status` varchar(20) NOT NULL
+  `status` varchar(20) NOT NULL,
+  `primary_key` mediumtext NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `tasks`
 --
 
-INSERT INTO `tasks` (`username`, `taskname`, `status`) VALUES
-('kalam', 'Drink Water', 'unfinished'),
-('kalam', 'Exercise', 'unfinished'),
-('mama', 'Wake up early in the morning', 'unfinished'),
-('shuvo', 'Attain Seminer in BITM', 'unfinished'),
-('shuvo', 'Talk to At Least One Stranger', 'unfinished'),
-('shuvo', 'Few Some time in Mandir', 'unfinished'),
-('rana', 'Meditate', 'unfinished'),
-('rana', 'gkhbjk', 'unfinished');
+INSERT INTO `tasks` (`username`, `taskname`, `status`, `primary_key`) VALUES
+('shuvo', 'A Head Phone', 'done', '2019454928'),
+('shuvo', 'An UPS', 'unfinished', '2019454928'),
+('shuvo', 'Nuts', 'unfinished', '2019454928'),
+('shuvo', 'To Do List', 'done', '2019461158'),
+('shuvo', 'CMS ( Blog Post Site )', 'unfinished', '2019461158'),
+('shuvo', 'Poll Site', 'unfinished', '2019461158'),
+('shuvo', 'Social Media Site', 'unfinished', '2019461158'),
+('shuvo', 'Voirob', 'unfinished', '2019461435'),
+('shuvo', 'Shariatpur', 'unfinished', '2019461435'),
+('shuvo', 'Comilla', 'unfinished', '2019461435'),
+('shuvo', 'A Keyboard', 'unfinished', '2019454928'),
+('shuvo', 'An Arm Chair', 'unfinished', '2019454928'),
+('shuvo', 'A Mouse', 'done', '2019454928'),
+('shuvo', 'A Monitor 19-27\"', 'unfinished', '2019454928'),
+('shuvo', '12GB SSD', 'unfinished', '2019454928'),
+('shuvo', 'E-Commerce Site', 'unfinished', '2019461158'),
+('shuvo', 'Eat Clean', 'unfinished', '2019454859'),
+('shuvo', 'Reduce Smart Phone Use and Internet Usage', 'unfinished', '2019454859'),
+('shuvo', 'Get a Job', 'unfinished', '2019454859'),
+('shuvo', 'Have Some Very Good Friends', 'unfinished', '2019454859'),
+('shuvo', 'Travel Every Visit-able Place in the City', 'unfinished', '2019454859'),
+('shuvo', 'Travel at least 20 districts', 'unfinished', '2019454859'),
+('shuvo', 'Get More Lean and Fit', 'unfinished', '2019454859'),
+('shuvo', 'Make An Epic Facebook Profile', 'unfinished', '2019454859'),
+('shuvo', 'Become a Badass Web Developer', 'unfinished', '2019454859'),
+('shuvo', 'Learn Violin', 'unfinished', '2019454859'),
+('shuvo', 'Read At Least 20 Books', 'unfinished', '2019454859'),
+('shuvo', 'Start a YouTube Channel', 'unfinished', '2019454859'),
+('shuvo', 'Know Thyself', 'unfinished', '2019454859'),
+('shuvo', 'Fix My Relation With my Family', 'unfinished', '2019454859'),
+('shuvo', 'Build a Garden', 'unfinished', '2019454859'),
+('shuvo', 'Avengers : End Game', 'unfinished', '2019452038'),
+('shuvo', 'Tare Zameen Par', 'unfinished', '2019452038'),
+('shuvo', 'Papilion', 'unfinished', '2019452038'),
+('shuvo', 'Lord of The Rings', 'unfinished', '2019452038'),
+('shuvo', 'A Touch Pad', 'unfinished', '2019454928'),
+('shuvo', 'The Great Escape', 'unfinished', '2019452038'),
+('shuvo', 'GodFather', 'unfinished', '2019452038'),
+('shuvo', 'Nowga', 'unfinished', '2019461435');
 
 -- --------------------------------------------------------
 
@@ -65,9 +121,11 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`username`, `password`, `id`) VALUES
-('shuvo', 'shuvo', 1),
 ('sojol', 'abcd', 2),
-('rana', 'rana', 2);
+('rana', 'rana', 2),
+('shuvo', 'shuvo', 2),
+('touhid', 'touhid', 2),
+('selim', 'selim', 2);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
